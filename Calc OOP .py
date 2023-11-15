6ead6eb48e4f627173670eee7f3eace3fb62826e
J-Thumi <145795204+J-Thumi@users.noreply.github.com>
2023-11-15 18:12:15 +0300

    Update Calc OOP .py

--- Calc OOP .py
+++ Calc OOP .py
@@ -1,16 +1,21 @@
 class Calculator:
-    def sum(self,x,y):
-        return x+y
-first=int(input("First: "))
-second=int(input("Second: "))
-cal=Calculator()
-print(cal.sum(first,second))
+    def __init__(self):
+        self.result = 0
+
+    def add(self, x, y):
+        self.result = x + y
+
+    def get_result(self):
+        return self.result
 
 
+calculator = Calculator()
+x = float(input("Enter the first number: "))
+y = float(input("Enter the second number: "))
+calculator.add(x, y)
 
+result = calculator.get_result()
 
-
-
-
+print(f"Result: {result}")
 
 
