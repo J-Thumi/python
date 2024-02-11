cb017ca4f7def9f6e3e50a64274875b88f7b9050
Josphat Thumi <145795204+J-Thumi@users.noreply.github.com>
2024-02-11 22:13:57 +0300

    Create rect.py

---
+++ rect.py
@@ -0,0 +1,28 @@
+
+
+for y in range(5):
+    if y==0 or y==4:
+        print(' ',end='')
+    else:
+        print('x',end='')
+        
+print()
+        
+for i in range(5):
+    for j in range(5):
+        
+        
+        
+        if j==0 or j==4:
+            print("X",end="")
+        else:
+            print('-',end="")
+    print()
+
+for y in range(5):
+    if y==0 or y==4:
+        print(' ',end='')
+    else:
+        print('x',end='')
+    
+        
