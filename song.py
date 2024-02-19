fd05b6237ae1c48f491ef43344ca9ae50f4e1fe0
Josphat Thumi <145795204+J-Thumi@users.noreply.github.com>
2024-02-19 14:56:36 +0300

    Create song.py

---
+++ song.py
@@ -0,0 +1,80 @@
+def die():
+    print("Perhaps she'll die.")
+def fly():
+    print("I don't know why she swallowed that fly,")
+def spider():
+    print("She swallowed the spider to catch the fly,")
+def bird():
+    print("She swallowed the bird to catch the spider,")
+def cat():
+    print("She swallowed the cat to catch the bird,")
+def dog():
+    print("She swallowed the dog to catch the cat,")
+def hog():
+    print("What a hog to swallow a dog.")
+def hyena():
+    print("She swallowed the hyena to catch the dog,")
+def one():
+    print("There was an old woman who swallowed a fly.")
+    fly()
+    die()
+def two():
+    print("There was an old woman who swallowed a spider,")
+    print("That wriggled and iggled and jiggled inside her.")
+    spider()
+    fly()
+    die()
+def three():
+    print("There was an old woman who swallowed a bird,")
+    print("How absurd to swallow a bird.")
+    bird()
+    spider()
+    fly()
+    die()
+def four():
+    print("There was an old woman who swallowed a cat,")
+    print("Imagine that to swallow a cat.")
+    cat()
+    bird()
+    spider()
+    fly()
+    die()
+def five():
+    print("There was an old woman who swallowed a dog,")
+    print("What a hog to swallow a dog.")
+    dog()
+    cat()
+    bird()
+    spider()
+    fly()
+    die()
+def six():
+    print("There was an old woman who swallowed a hyena,")
+    print("Haa a hyena to swallow a dog,")
+    hyena() 
+    dog()
+    cat()
+    bird()
+    spider()
+    fly()
+    die()
+def seven():
+    print("There was an old woman who swallowed a horse,")
+    print("She died of course.")
+one()
+print()
+two()
+print()
+three()
+print()
+four()
+print()
+five()
+print()
+six()
+print()
+seven()
+
+
+
+    
